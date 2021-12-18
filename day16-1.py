f4f457918f8cc5e48c4a3f07f4d023d577dac9d0
Ken Leung <kenleung5e28@gmail.com>
2021-12-18 15:07:25 +0800

    add day 16

---
+++ day16-1.py
@@ -0,0 +1,87 @@
+def parse_literal(s: str, start: int) -> tuple[int, int]:
+  p, num = start, ''
+  while True:
+    done = s[p] == '0'
+    num += s[(p + 1):(p + 5)]
+    p += 5
+    if done:
+      break
+  return int(num, base=2), p
+
+def next_ptr(p: int) -> int:
+  mod = p % 4
+  offset = 0 if mod == 0 else 4 - mod
+  return p + offset
+
+
+def parse_packet(s: str, start: int) -> tuple[dict, int]:
+  p = start
+  version = int(s[p:(p + 3)], base=2)
+  p += 3
+  id = int(s[p:(p + 3)], base=2)
+  p += 3
+  isLiteral = id == 4
+  packet = {
+    'version': version,
+    'id': id,
+    'isLiteral': isLiteral,
+  }
+  if isLiteral:
+    value, p = parse_literal(s, p)
+    return packet | {
+      'value': value,
+    }, p
+  else:
+    subpackets = []
+    if s[p] == '0':
+      p += 1
+      length = int(s[p:(p + 15)], base=2)
+      p += 15
+      end = p + length
+      while p < end:
+        subpacket, p = parse_packet(s, p)
+        subpackets.append(subpacket)
+    else:
+      p += 1
+      count = int(s[p:(p + 11)], base=2)
+      p += 11
+      for _ in range(count):
+        subpacket, p = parse_packet(s, p)
+        subpackets.append(subpacket)
+    return packet | {
+      'subpackets': subpackets
+    }, p
+
+digits = {
+  '0': '0000',
+  '1': '0001',
+  '2': '0010',
+  '3': '0011',
+  '4': '0100',
+  '5': '0101',
+  '6': '0110',
+  '7': '0111',
+  '8': '1000',
+  '9': '1001',
+  'A': '1010',
+  'B': '1011',
+  'C': '1100',
+  'D': '1101',
+  'E': '1110',
+  'F': '1111',
+}
+input = None
+with open('input-day16.txt') as file:
+  input = file.readline().rstrip()
+
+def parse(s: str) -> dict:
+  return parse_packet(''.join([digits[c] for c in s]), 0)[0]
+
+def version_sum(packet: dict) -> int:
+  sum = packet['version']
+  if not packet['isLiteral']:
+    for subpacket in packet['subpackets']:
+      sum += version_sum(subpacket)
+  return sum
+
+print(version_sum(parse(input)))
